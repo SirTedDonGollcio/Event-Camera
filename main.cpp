7e42c1044907edfa56121cd4be3f89ee787c75eb
SirTedDonGollcio <tadeusz.pawel.golczyk@gmail.com>
2023-01-19 10:56:56 +0100

    Event camera events are being summed in the progresive variable

--- main.cpp
+++ main.cpp
@@ -4,19 +4,102 @@
 
 using namespace cv;
 using namespace std;
+
+class EventCamera
+{
+    public:
+        Mat frame1_t, frame2_t;
+        VideoCapture* camera;
+        vector<int**> getDiff()
+        {
+            
+            frame2_t = frame1_t.clone();
+            camera->read(frame1_t);
+
+
+
+            int** temp_mat_B = new int* [frame1_t.rows];
+
+            for (int i = 0; i < frame1_t.rows; i++) {
+
+                temp_mat_B[i] = new int[frame1_t.cols];
+            }
+            int** temp_mat_G = new int* [frame1_t.rows];
+
+            for (int i = 0; i < frame1_t.rows; i++) {
+
+                temp_mat_G[i] = new int[frame1_t.cols];
+            }
+            int** temp_mat_R = new int* [frame1_t.rows];
+
+            for (int i = 0; i < frame1_t.rows; i++) {
+
+                temp_mat_R[i] = new int[frame1_t.cols];
+            }
+
+            int ttt1, ttt2, ttt3;
+
+
+            int threshold_value = 15; //Value of the threshold under which the differences between each frames will be negleted
+
+
+            for (int i = 0; i < frame1_t.cols; i++)
+            {
+                for (int j = 0; j < frame1_t.rows; j++)
+                {
+                    ttt1 = (int)frame1_t.at<Vec3b>(j, i)[0] - (int)frame2_t.at<Vec3b>(j, i)[0];
+                    ttt2 = (int)frame1_t.at<Vec3b>(j, i)[1] - (int)frame2_t.at<Vec3b>(j, i)[1];
+                    ttt3 = (int)frame1_t.at<Vec3b>(j, i)[2] - (int)frame2_t.at<Vec3b>(j, i)[2];
+
+                    
+
+                    if (abs(ttt1) < threshold_value)
+                    {
+                        temp_mat_B[j][i] = 0;
+                    }
+                    else
+                    {
+                        temp_mat_B[j][i] = ttt1;
+                    }
+
+                    if (abs(ttt2) < threshold_value)
+                    {
+                        temp_mat_G[j][i] = 0;
+                    }
+                    else
+                    {
+                        temp_mat_G[j][i] = ttt2;
+                    }
+
+                    if (abs(ttt3) < threshold_value)
+                    {
+                        temp_mat_R[j][i] = 0;
+                    }
+                    else
+                    {
+                        temp_mat_R[j][i] = ttt3;
+                    }
+
+                }
+            }
+            vector <int**> mat_BGR{ temp_mat_B ,temp_mat_G ,temp_mat_R };
+            return mat_BGR;
+        }
+
+};
+
 int main(int, char**)
 {
-    Mat frame1, frame1_g;
-    Mat frame2, frame2_g;
+    Mat frame1,frame2;
     Mat difference, difference2;
-    Mat difference_init, difference_init2, BlackScreen, sum1;
-    Mat thresh;
-    vector<vector<Point> > cnts;
+    Mat difference_init, difference_init2, BlackScreen, sum1, sum2;
 
     VideoCapture cap;
 
     int deviceID = 0;
     int apiID = cv::CAP_ANY;
+    
+    //Preffered camera capture size
     cap.set(3, 512);
     cap.set(4, 288);
     cap.open(deviceID, apiID);
@@ -31,30 +114,64 @@ int main(int, char**)
     Mat in[] = { BlackScreen_temp, BlackScreen_temp, BlackScreen_temp };
     merge(in, 3, BlackScreen);
 
-  
-    absdiff(frame1, BlackScreen, difference_init);
-    threshold(difference_init, difference_init2, 5, 255, cv::THRESH_TOZERO);
 
+    EventCamera ec;
+    ec.camera = &cap;
+    ec.frame1_t = frame1;
+    ec.frame2_t = BlackScreen;
 
+    sum1,sum2 = BlackScreen;
+    difference = BlackScreen;
 
-
-    cap.read(frame2);
+    vector<int**> mats;
     for (;;)
     {
-        frame2 = frame1.clone();
-        cap.read(frame1);
+        mats = ec.getDiff();
+
+        sum1 = sum2.clone();
+
+        for (int i = 0; i < sum1.cols; i++)
+        {
+            for (int j = 0; j < sum1.rows; j++)
+            {
+                if ((int)sum1.at<Vec3b>(j, i)[0] + (int)mats[0][j][i] > 255)
+                {
+                    sum2.at<Vec3b>(j, i)[0] = 255;
+                }
+                else
+                {
+                    sum2.at<Vec3b>(j, i)[0] = (int)sum1.at<Vec3b>(j, i)[0] + (int)mats[0][j][i];
+                }
+
+                if ((int)sum1.at<Vec3b>(j, i)[1] + (int)mats[1][j][i] > 255)
+                {
+                    sum2.at<Vec3b>(j, i)[1] = 255;
+                }
+                else
+                {
+                    sum2.at<Vec3b>(j, i)[1] = (int)sum1.at<Vec3b>(j, i)[1] + (int)mats[1][j][i];
+                }
+
+                if ((int)sum1.at<Vec3b>(j, i)[2] + (int)mats[2][j][i] > 255)
+                {
+                    sum2.at<Vec3b>(j, i)[2] = 255;
+                }
+                else
+                {
+                    sum2.at<Vec3b>(j, i)[2] = (int)sum1.at<Vec3b>(j, i)[2] + (int)mats[2][j][i];
+                }
+
+                difference.at<Vec3b>(j, i)[0] = (int)mats[0][j][i];
+                difference.at<Vec3b>(j, i)[1] = (int)mats[1][j][i];
+                difference.at<Vec3b>(j, i)[2] = (int)mats[2][j][i];
+            }
+        }
+
+
+        imshow("Just difference", sum2);
+        imshow("Frame + difference", difference);
+
         waitKey(1);
-
-        absdiff(frame2, frame1, difference);
-
-        //Prog ponizej ktorego usuwa roznice znalezione pomiedzy kolejnymi klatkami
-        threshold(difference, difference2, 5, 255, cv::THRESH_TOZERO);
-        
-        sum1 = frame2 + difference2;
-        imshow("Just difference", difference2);
-        imshow("Frame + difference",sum1);
-
-        
 
         
         if (waitKey(1) >= 0)
